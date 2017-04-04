1c6dc90c10457cd595f86883a1b1aedc1aaaf03e
Steve Coward <steve.coward@gmail.com>
2017-04-04 14:03:17 -0400

    adds todict helper method.

--- nasl_parser/__init__.py
+++ nasl_parser/__init__.py
@@ -55,3 +55,44 @@ class NaslScript():
                         raise Exception('Unable to match regex pattern.')
                 except:
                     pass
+
+    def _todict(self, pretty=False):
+        return {
+            'ID': self.id,
+            'Name': self.name,
+            'Summary': self.summary,
+            'Version': self.version,
+            'CVS Date': self.cvs_date,
+            'CVE IDs': self.cve_id,
+            'CWE IDs': self.cwe_id,
+            'Bugtraq IDs': self.bugtraq_id,
+            'OSVDB IDs': self.osvdb_id,
+            'Vulnerability Family': self.family,
+            'Copyright': self.copyright,
+            'XREFs': self.xref,
+            'Attributes': self.set_attribute,
+            'CVSS Base Vector': self.set_cvss_base_vector,
+            'CVSS Temporal Vector': self.set_cvss_temporal_vector,
+            'CVSS3 Base Vector': self.set_cvss3_base_vector,
+            'CVSS3 Temporal Vector': self.set_cvss3_temporal_vector,
+            'Category': self.category,
+        } if pretty else {
+            'id': self.id,
+            'name': self.name,
+            'summary': self.summary,
+            'version': self.version,
+            'cvs_date': self.cvs_date,
+            'cve_id': self.cve_id,
+            'cwe_id': self.cwe_id,
+            'bugtraq_id': self.bugtraq_id,
+            'osvdb_id': self.osvdb_id,
+            'family': self.family,
+            'copyright': self.copyright,
+            'xref': self.xref,
+            'attributes': self.set_attribute,
+            'cvss_base_vector': self.set_cvss_base_vector,
+            'cvss_temporal_vector': self.set_cvss_temporal_vector,
+            'cvss3_base_vector': self.set_cvss3_base_vector,
+            'cvss3_temporal_vector': self.set_cvss3_temporal_vector,
+            'category': self.category,
+        }
