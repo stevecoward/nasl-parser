a7c6e444f604cbdc35d0fcac37b01153217cfa86
Steve Coward <steve.coward@gmail.com>
2017-04-04 14:04:38 -0400

    bumps version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
       name='nasl-parser',
-      version='0.1.2',
+      version='0.1.3',
       author='Steve Coward',
       author_email='steve.coward@gmail.com',
       url='https://github.com/stevecoward/nasl-parser',
